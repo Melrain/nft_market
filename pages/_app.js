c41cfd1ee140b3dfa7c9475e1f5ce7e6665253ac
Melrain <yezyd92@icloud.com>
2023-09-05 10:15:53 +0800

    complete pages's structure

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,17 @@
-const App = () => <div>App</div>;
+import React from 'react';
+import App from 'next/app';
 
-export default App;
+class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <>
+        {/* 可以在此添加页面共享的布局或导航栏 */}
+
+        <Component {...pageProps} />
+      </>
+    );
+  }
+}
+
+export default MyApp;
--- pages/create-nft.js
+++ pages/create-nft.js
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const CreateNFT = () => <div>create-nft</div>;
 
 export default CreateNFT;
