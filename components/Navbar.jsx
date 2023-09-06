ce8a3863543a3786dc971305d5c9a9fb0762c59d
Melrain <yezyd92@icloud.com>
2023-09-06 10:45:59 +0800

    finish navbar on lg

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,14 +3,79 @@ import { useRouter } from 'next/router';
 import { useTheme } from 'next-themes';
 import Image from 'next/image';
 import Link from 'next/link';
-
 import images from '../assets';
+import Button from './Button';
+
+const MenuItems = ({ isMobile, active, setActive }) => {
+  const generateLink = (index) => {
+    switch (index) {
+      case 0:
+        return '/';
+      case 1:
+        return 'created-nfts';
+      case 2:
+        return 'my-nfts';
+      default:
+        return '/';
+    }
+  };
+
+  return (
+    <ul
+      className={`list-none flexCenter flex-row ${
+        isMobile && 'flex-col h-full'
+      }`}
+    >
+      {['Explore NFTs', 'Listed NFTs', 'My NFTs'].map((item, index) => (
+        <li
+          key={index}
+          onClick={() => {
+            setActive(item);
+          }}
+          className={`flex flex-row items-center font-poppins font-semibold text-base dark:hover:text-white hover:text-nft-dark mx-3 ${
+            active === item
+              ? 'dark:text-white text-nft-black-1'
+              : 'dark:text-nft-gray-3 text-nft-gray-2'
+          }`}
+        >
+          <Link href={generateLink(index)}>{item}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+const ButtonGroup = ({ setActive, router }) => {
+  const hasConnected = true;
+
+  return hasConnected ? (
+    <Button
+      btnName="Create"
+      classStyles="mx-2 rounded-xl"
+      handleClick={() => {
+        setActive('');
+        router.push('/create-nft');
+      }}
+    />
+  ) : (
+    <Button
+      btnName="Connect"
+      classStyles="mx-2 rounded-xl"
+      handleClick={() => {}}
+    />
+  );
+};
 
 const Navbar = () => {
   const { theme, setTheme } = useTheme();
-  console.log({ theme });
+  const router = useRouter();
+  const [active, setActive] = useState('Explore NFTs');
+  // 控制小屏幕的菜单打开和关闭
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav className="flexBetween w-full fixed z-10 p-4 flex-row border-b dark:bg-nft-dark bg-white dark:border-nft-black-1 border-nft-gray-1">
+      {/* 左侧logo */}
       <div className="flex flex-1 flex-row justify-start">
         <Link href="/">
           <div
@@ -41,6 +106,39 @@ const Navbar = () => {
           </div>
         </Link>
       </div>
+      {/* 右侧内容-LG */}
+      <div className="flex flex-initial flex-row justify-end">
+        {/* 深浅开关 */}
+        <div className="flex items-center mr-2">
+          <input
+            type="checkbox"
+            className="checkbox"
+            id="checkbox"
+            onChange={() => {
+              setTheme(theme === 'light' ? 'dark' : 'light');
+            }}
+          />
+          <label
+            htmlFor="checkbox"
+            className="flexBetween w-10 h-5 bg-black rounded-2xl p-1 relative label"
+          >
+            <i className="fas fa-sun " />
+            <i className="fas fa-moon" />
+
+            <div className="w-4 h-4 absolute bg-white rounded-full ball" />
+          </label>
+        </div>
+        {/* MenuItems */}
+        <div className="md:hidden flex">
+          <MenuItems isMobile={false} active={active} setActive={setActive} />
+          <div className="ml-4">
+            <ButtonGroup setActive={setActive} router={router} />
+          </div>
+        </div>
+      </div>
+
+      {/* 右侧内容-sm */}
+      <div className="hidden md:flex ml-2"> </div>
     </nav>
   );
 };
